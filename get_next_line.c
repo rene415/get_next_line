fd3d2ab61a501612a011c0aad32e95abb6514855
Rene Ramirez <rramirez@e1z2r6p3.42.us.org>
2017-03-11 17:05:34 -0800

    files needed

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,18 @@
 /*   By: rramirez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 20:18:53 by rramirez          #+#    #+#             */
-/*   Updated: 2017/03/10 20:18:55 by rramirez         ###   ########.fr       */
+/*   Updated: 2017/03/11 17:04:37 by rramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+static something(const int fd, char **file, )
+
+int		get_next_line(const int fd, char **file)
+{
+	int i;
+	int j;
+
+
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,21 @@
 /*   By: rramirez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 20:18:40 by rramirez          #+#    #+#             */
-/*   Updated: 2017/03/10 20:18:48 by rramirez         ###   ########.fr       */
+/*   Updated: 2017/03/11 17:04:42 by rramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define
+#ifndef	GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include
+#include
+#include
+
+#define BUFF_SIZE		1
+#define TRUE			1
+#define	FALSE			0
+
+int		get_next_line(const int	fd, char ** line);
+
+#endif
